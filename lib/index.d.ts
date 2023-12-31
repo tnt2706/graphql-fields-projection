ee24ea1ddb12c30b74919805ca2f814a1f91ac92
Tran Tinh <tinhtranngoc@itrvn.com>
2023-12-31 12:16:59 +0700

    fix bugs

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -3,27 +3,27 @@
  *
  * @param {Object} queryInfo query info
  * @param {Object} options options
- * @param {[String]} [options.additionalFields=[]] additional fields to get
+ * @param {String[]} [options.additionalFields=[]] additional fields to get
  * @param {String} [options.path=''] child path of query
  * @param {String} [options.returnType='string'] must be either: string, array, or object. Default: string
  * @returns
  */
 export function createSelectedFields(queryInfo: any, options?: {
-    additionalFields?: [string];
+    additionalFields?: string[];
     path?: string;
     returnType?: string;
 }): any;
 /**
  * Create merged selected fields. Useful for dataloader batch function
  *
- * @param {[String]} batchingKeys dataloader batching keys. Each element must be a JSON string
+ * @param {String []} batchingKeys dataloader batching keys. Each element must be a JSON string
  * @param {Object} options options
- * @param {[String]} [options.additionalFields=[]] additional fields to get
+ * @param {String []} [options.additionalFields=[]] additional fields to get
  * @param {String} [options.returnType='string'] must be either: string, array, or object. Default: string
  * @returns
  */
-export function createMergedSelectedFields(batchingKeys: [string], options?: {
-    additionalFields?: [string];
+export function createMergedSelectedFields(batchingKeys: string[], options?: {
+    additionalFields?: string[];
     returnType?: string;
 }): {
     ids: any;
--- lib/utils.d.ts
+++ lib/utils.d.ts
@@ -1,17 +1,16 @@
 /**
  * Remove Path Collision from input fields
  *
- * @param {[string]} fields input fields
+ * @param {string[]} fields input fields
  * @param {string} [returnType='array'] must be either: string, array, or object. Default: array
  * @returns fields projection
- * @author TrongPham KhoaNgo
  */
-export function removePathCollision(fields?: [string], returnType?: string): any;
+export function removePathCollision(fields?: string[], returnType?: string): any;
 /**
  *
  * create the return value
- * @param {[string]} fields input fields
+ * @param {string[]} fields input fields
  * @param {string} [returnType='array'] must be either: string, array, or object. Default: array
  * @returns
  */
-export function createResult(fields: [string], returnType?: string): any;
+export function createResult(fields: string[], returnType?: string): any;
